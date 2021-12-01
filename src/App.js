856f1b10b03282a3504cca27ee81a650149262fe
Jethro Williams <jethrowilliams@outlook.com>
2021-12-01 16:38:31 +0000

    Check whether a user is logged-in with useEffect() on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+
 import Dashboard from './components/Dashboard';
 import Home from './components/Home';
 
@@ -7,10 +9,35 @@ const App = () => {
   const [loggedInStatus, setLoggedInStatus] = useState('NOT_LOGGED_IN');
   const [user, setUser] = useState({});
 
+  const checkLoginStatus = () => {
+    axios
+      .get('http://localhost:3001/logged_in', { withCredentials: true })
+      .then(response => {
+        console.log('🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉🙉');
+        console.log(response);
+
+        if (response.data.logged_in && loggedInStatus === 'NOT_LOGGED_IN') {
+          setLoggedInStatus('LOGGED_IN');
+          setUser(response.data.user);
+        } else if (!response.data.logged_in && loggedInStatus === 'LOGGED_IN') {
+          setLoggedInStatus('NOT_LOGGED_IN');
+          setUser({});
+        }
+      })
+      .catch(error => {
+        console.log('💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀💀');
+        console.log(error);
+      });
+  };
+
   const handleLogin = data => {
     setLoggedInStatus('LOGGED_IN');
     setUser(data.user);
   };
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
 
   return (
     <div>
